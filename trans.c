e4635da2dbe382de36c6410458074bc95e28584a
anusiya-m28 <anusiya.m_25it@kgkite.ac.in>
2026-05-17 20:09:38 +0530

    changes made

--- trans.c
+++ trans.c
@@ -81,19 +81,20 @@ void textFile(FILE *readPtr)
         fprintf(writePtr, "%-6s%-16s%-11s%10s\n", "Acct", "Last Name", "First Name", "Balance");
 
         // copy all records from random-access file into text file
-        while (!feof(readPtr))
-        {
-            result = fread(&client, sizeof(struct clientData), 1, readPtr);
+    while ((result = fread(&client, sizeof(struct clientData), 1, readPtr)) == 1)
+{
+    if (client.acctNum != 0)
+    {
+        fprintf(writePtr, "%-6d%-16s%-11s%10.2f\n",
+                client.acctNum,
+                client.lastName,
+                client.firstName,
+                client.balance);
+        }//end if
+    } //end while
+   
 
-            // write single record to text file
-            if (result != 0 && client.acctNum != 0)
-            {
-                fprintf(writePtr, "%-6d%-16s%-11s%10.2f\n", client.acctNum, client.lastName, client.firstName,
-                        client.balance);
-            } // end if
-        }     // end while
-
-        fclose(writePtr); // fclose closes the file
+     fclose(writePtr); // fclose closes the file
     }                     // end else
 } // end function textFile
 
